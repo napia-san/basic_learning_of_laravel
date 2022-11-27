7871931b121e96e7b6e129343b651d62107c5d59
napia-san <bn18250@shibaura-it.ac.jp>
2022-11-27 05:53:58 +0000

    管理画面の認証と認可の実装

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminLoginPostRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /*
+    * display "top page"
+    */
+    public function index()
+    {
+        return view('admin.index');
+    }
+    
+    /**
+     * login
+     */
+    public function login(AdminLoginPostRequest $request)
+    {
+        // validate済
+
+        // データの取得
+        $datum = $request->validated();
+        //var_dump($datum); exit;
+
+        // 認証
+        if (Auth::guard('admin')->attempt($datum) === false) {
+            return back()
+                   ->withInput() // 入力値の保持
+                   ->withErrors(['auth' => 'ログインIDかパスワードに誤りがあります。',]) // エラーメッセージの出力
+                   ;
+        }
+
+        //
+        $request->session()->regenerate();
+        return redirect()->intended('/admin/top');
+    }
+    
+    /*
+    * logout
+    */
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+        $request->session()->regenerateToken();  // CSRFトークンの再生成
+        $request->session()->regenerate();  // セッションIDの再生成
+        return redirect(route('admin.index'));
+    }
+}
